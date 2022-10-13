aa0143bf26f27d2ad1a85243077bc613adfed673
emefielezn <emefielengozi54@gmail.com>
2022-10-13 10:28:19 -0700

    added assembler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main: Entry
+* Description: Programming is like building a multilingual puzzle
+* Return: Always o (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
