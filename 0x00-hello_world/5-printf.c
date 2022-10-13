fb2d23f7a238e6797ef6d4aec9a5bfed4bffb217
emefielezn <emefielengozi54@gmail.com>
2022-10-13 14:05:45 -0700

    sizes of various types

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/**
-* Main: Entry point
-* Description: prints with proper grammar, but the outcome is a piece of art
-* Return: 0 (success)
-*/
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art");
-	return (0);
-}
+	#include <stdio.h>
+	/**
+	* Main -  print text
+	* Description: prints with proper grammar, but the outcome is a piece of art
+	* Return: 0 (success)
+	*/
+	int main(void)
+	{	
+		printf("with proper grammar, but the outcome is a piece of art");
+		return (0);
+	}
