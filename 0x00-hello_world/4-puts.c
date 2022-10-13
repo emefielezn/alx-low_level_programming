bf7cdd09b19bf5e7b8ec38930f5312859a20b51d
emefielezn <emefielengozi54@gmail.com>
2022-10-13 13:59:32 -0700

    sizes of various types

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-/**
- *  main - to print an exact string 
- *
- *  Description: Programming is like building a multilingual puzzle
- *  Return: Always 0 (success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+	#include <stdio.h>
+	/**
+	 *  main - to print an exact string 
+	 *
+	 *  Description: Programming is like building a multilingual puzzle
+	 *  Return: Always 0 (success)
+	 */
+	int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
