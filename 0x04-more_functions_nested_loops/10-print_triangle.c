715626573f0ee2e67b530ec078c1468003da65a5
emefielezn <emefielengozi54@gmail.com>
2022-10-20 16:32:53 -0700

    edited tast10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,27 +3,24 @@
 /**
  * print_triangle - prints a triangle
  * @size:size parameter of triangle
+ *
  * Return: returns nothing
  */
 void print_triangle(int size)
 {
-	int inc1, inc2;
+	int inc1, inc2, inc3;
 
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			for (inc2 = 1; incr2 <= size - inc1; inc2++)
 			{
 				putchar(' ');
 			}
-			for (inc2 = 0; inc2 < inc1; inc2++)
+			for (inc3 = 0; (inc3 != (2*incr1-1)); inc3++)
 			{
 				putchar('#');
-			}
-			if (inc1 == size)
-			{
-				continue;
 			}
 		}
 	}
