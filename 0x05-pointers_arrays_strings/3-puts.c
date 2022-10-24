f2a648593fb1e7cd6258ee18e0ba7a762689a35a
emefielezn <emefielengozi54@gmail.com>
2022-10-24 15:17:45 -0700

    create some functions

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string to print
+ * On success: returns no error
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
