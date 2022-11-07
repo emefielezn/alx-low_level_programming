894b659e0e6d8b43d22eec8b3b5c3435e5c0e1d8
emefielezn <emefielengozi54@gmail.com>
2022-11-07 06:41:42 -0800

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -54,12 +54,12 @@ int num_words(char *str)
  */
 char **strtow(char *str)
 {
-	char **split
+	char **split;
 	int i, j = 0, temp = 0, size = 0, words = num_words(str);
 
 	if (words == 0)
 		return (NULL);
-	split = (char **)malloc(sizeof(char *) * (words + 1));
+	split = (char **) malloc(sizeof(char *) * (words + 1));
 	if (split != NULL)
 	{
 		for (i = 0; i <= len(str) && words; i++)
