f8008b23980dd4bab97724646e1948ead2715132
emefielezn <emefielengozi54@gmail.com>
2022-11-14 15:01:43 -0800

    edited task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,9 +15,7 @@ char *_copy(char *src)
 		return (NULL);
 	}
 	for (len = 0; src[len] != '\0'; len++)
-	{
-		ptr = malloc(sizeof(char) * (len + 1));
-	}
+	ptr = malloc(sizeof(char) * (len + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
